29050b26c6f3429a5581611980b6ce096d196aca
Ma <212808041@logon.ds.ge.com>
2020-06-17 17:36:03 -0400

    another another attempt to fix cors

--- learning.js
+++ learning.js
@@ -42,12 +42,12 @@
     // Download the data and push to table object 
     myConnector.getData = function (table, doneCallback) {
         var api_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641";
-        console.log("test3")
+        console.log("test4")
         
 
         //change the url here so that its pointing to your API 
-        $.getJSON("https://PITC-Zscaler-Americas-Cincinnati3PR.proxy.corporate.ge.com:80/https://ge-dw.aha.io/api/v1/bookmarks/custom_pivots/6837530641511890383/6838682896900871649?view=list?access_token=" + api_key, function (resp) {
-       //     resp.set('Access-Control-Allow-Origin', '*');
+        $.getJSON("https://ge-dw.aha.io/api/v1/bookmarks/custom_pivots/6837530641511890383/6838682896900871649?view=list?access_token=" + api_key, function (resp) {
+            resp.set('Access-Control-Allow-Origin', '*');
             var feat = resp.columns, //features should be whatever the object in the json file is 
                 tableData = [];
 
