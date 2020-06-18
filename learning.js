7d3679d736f54e2c8d1fb7d11e008a9055c2dc10
Ma <212808041@logon.ds.ge.com>
2020-06-18 11:35:43 -0400

    changed ajax

--- learning.js
+++ learning.js
@@ -42,11 +42,48 @@
     // Download the data and push to table object 
     myConnector.getData = function (table, doneCallback) {
         var api_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641";
-        console.log("test4")
-        
+        console.log("test5")
+
+       /* $.ajaxSetup({
+            headers: {
+                '': 'key_here'
+            }
+        });
+        */
+        var settings = {
+            "url": "https://ge-dw.aha.io/api/v1/bookmarks/custom_pivots/6837530641511890383?view=list/APP-1?",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "Authorization": "Bearer 6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641",
+                "Cookie": "_aha_t=6839706221717396714"
+            },
+        };
+
+        $.ajax(settings).done(function (response) {
+            var feat = response.columns, //features should be whatever the object in the json file is 
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    //tells the mapping from the defined fields to the schema 
+                    //maps the different objects in the json response to the schema you defined
+                    "ReleaseName": feat[i].title,
+                    "OriginalGADate": feat[i].title,
+                    "ReleaseDate": feat[i].title,
+                    "WorkspaceName": feat[i].title,
+                    "Productline": feat[i].title,
+                    "ReleaseType": feat[i].title,
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
 
         //change the url here so that its pointing to your API 
-        $.getJSON("https://ge-dw.aha.io/api/v1/bookmarks/custom_pivots/6837530641511890383/6838682896900871649?view=list?access_token=" + api_key, function (resp) {
+      /*  $.getJSON("https://ge-dw.aha.io/api/v1/bookmarks/custom_pivots/6837530641511890383/6838682896900871649?view=list?access_token=" + api_key, function (resp) {
             resp.set('Access-Control-Allow-Origin', '*');
             var feat = resp.columns, //features should be whatever the object in the json file is 
                 tableData = [];
@@ -68,6 +105,7 @@
             table.appendRows(tableData);
             doneCallback();
         });
+        */
     };
 
     tableau.registerConnector(myConnector);
