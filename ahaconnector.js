cc67063e2682c5d89ae772db7768e7be1f4130b6
Chenhao Ma <chenhao.ma@ge.com>
2020-06-30 14:53:05 -0400

    seeing if it works with api key input field

--- ahaconnector.js
+++ ahaconnector.js
@@ -38,8 +38,8 @@
     };
     // Download the data and push to table object 
     myConnector.getData = function (table, doneCallback) {
-
-        var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account 
+        var auth_key = document.getElementById("api-key")
+      //  var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account 
         var list_id = "6837530641511890383"; //id for the specific view - is the first set of numbers after custom_pivots 
   
         var settings = {
