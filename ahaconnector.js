f9fe0d4d99d668e9c3032bf1540b6947babf663a
Chenhao Ma <chenhao.ma@ge.com>
2020-06-30 16:58:58 -0400

    trying to figure out api key issue

--- ahaconnector.js
+++ ahaconnector.js
@@ -40,11 +40,11 @@
     // Download the data and push to table object 
     myConnector.getData = function (table, doneCallback) {
         console.log(tableau.connectionData);
-        var auth_key = tableau.connectionData;
-        console.log(auth_key)
-        console.log(typeof auth_key)
+       // var auth_key = tableau.connectionData;
+        //console.log(auth_key)
+        //console.log(typeof auth_key)
 
-        // var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account
+        var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account
         var list_id = "6837530641511890383"; //id for the specific view - is the first set of numbers after custom_pivots 
   
         var settings = {
@@ -52,7 +52,7 @@
             "method": "GET",
             "timeout": 0,
             "headers": {
-                "Authorization": "Bearer " + "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641",
+                "Authorization": "Bearer " + auth_key,
             },
         };
 
