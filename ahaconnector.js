6c4492f7222467bcbfa33dd2bae043d6176d5a5e
Chenhao Ma <chenhao.ma@ge.com>
2020-06-30 16:33:49 -0400

    added the passing of data through the different tableau phases

--- ahaconnector.js
+++ ahaconnector.js
@@ -39,9 +39,8 @@
     };
     // Download the data and push to table object 
     myConnector.getData = function (table, doneCallback) {
-        var auth_key = document.getElementById("api-key").value
-        console.log(auth_key)
-      //  var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account 
+        var auth_key = tableau.connectionData;
+       // var auth_key = "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641"; //can use this one or just create another one from your aha account 
         var list_id = "6837530641511890383"; //id for the specific view - is the first set of numbers after custom_pivots 
   
         var settings = {
@@ -49,7 +48,7 @@
             "method": "GET",
             "timeout": 0,
             "headers": {
-                "Authorization": "Bearer " + "6ca8cd130f172749edff6c83ad90630ec3d1b081dd5b8570226c8f3fa087b641",
+                "Authorization": "Bearer " + auth_key,
             },
         };
 
@@ -81,7 +80,12 @@
 
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
-        $("#submitButton").click(function() {
+        $("#submitButton").click(function () {
+
+            var auth_key = $('#api-key').val().trim();
+            tableau.connectionData = JSON.stringify(auth_key);
+
+
             tableau.connectionName = "Aha! Connection Feed"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
